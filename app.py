f209ecd307b7741c816a0cb25ca25a1d88eba13f
ayeshareh <ayesharehmat48208@gmail.com>
2025-12-01 14:25:56 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 # GEMINI API CONFIG
 # -------------------------------
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MODEL = "gemini-2.5-flash"
+MODEL = "gemini-2.0-flash"
 GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL}:generateContent?key={GEMINI_API_KEY}"
 
 # -------------------------------
